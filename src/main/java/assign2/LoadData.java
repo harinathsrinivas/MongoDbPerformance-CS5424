da70971c8e24c9bb2f9b0ba6b0df38fb85e78a2f
Michael <michael_chia7@msn.com>
2019-11-06 23:49:38 +0800

    shardkey hashed

--- src/main/java/assign2/LoadData.java
+++ src/main/java/assign2/LoadData.java
@@ -23,7 +23,7 @@ public class LoadData {
 	private static final String DEFAULT_MONGOIMPORT_PATH = "/temp/MongoDb/mongo/mongos/mongodb-linux-x86_64-rhel70-4.2.1/bin/mongoimport";
 	private static final String DEFAULT_DATA_PATH = "project-files/data-files/";
 	private static final String HOST = "192.168.56.159";
-	private static final String NUM_WORKERS = "8";
+	private static final String NUM_WORKERS = "16";
 
 	
 	//private static final String DEFAULT_MONGOIMPORT_PATH = "/usr/local/bin/mongoimport";
