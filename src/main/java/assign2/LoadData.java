0d7f07101e16c7fac9988f7d2d0598b6193075f1
Michael <michael_chia7@msn.com>
2019-11-07 23:31:26 +0800

    clean

--- src/main/java/assign2/LoadData.java
+++ src/main/java/assign2/LoadData.java
@@ -21,13 +21,13 @@ import ch.qos.logback.classic.LoggerContext;
 public class LoadData {
 	public static final String DATABASE = "wholesale";
 	private static final String DEFAULT_DATA_PATH = "project-files/data-files/";
-	//private static final String DEFAULT_MONGOIMPORT_PATH = "/temp/MongoDb/mongo/mongos/mongodb-linux-x86_64-rhel70-4.2.1/bin/mongoimport";
-	//private static final String HOST = "192.168.56.159";
-	//private static final String NUM_WORKERS = "24";
+	private static final String DEFAULT_MONGOIMPORT_PATH = "/temp/MongoDb/mongo/mongos/mongodb-linux-x86_64-rhel70-4.2.1/bin/mongoimport";
+	private static final String HOST = "192.168.56.159";
+	private static final String NUM_WORKERS = "24";
 
-	private static final String DEFAULT_MONGOIMPORT_PATH = "/usr/local/bin/mongoimport";
-	private static final String HOST = "localhost";
-	private static final String NUM_WORKERS = "4";
+	//private static final String DEFAULT_MONGOIMPORT_PATH = "/usr/local/bin/mongoimport";
+	//private static final String HOST = "localhost";
+	//private static final String NUM_WORKERS = "4";
 
 	
 	private String mongoimportPath;
