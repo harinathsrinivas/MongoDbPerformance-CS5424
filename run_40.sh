5dc20ce72222d359eccddb789496c21fc36f853f
Michael <michael_chia7@msn.com>
2019-11-08 09:53:32 +0800

    run40

--- run_40.sh
+++ run_40.sh
@@ -5,7 +5,7 @@ do
     for j in majority,majority 1,3; 
     do 
         IFS=","
-        set -- $j;"
+        set -- $j;
         java -cp target/*:target/dependency/*:. assign2.Experiment $i $1 $2
     done
 done
